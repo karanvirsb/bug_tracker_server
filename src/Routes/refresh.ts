c18090161d77d515695074dcf2b97119ad59fc1d
mrboparai <karanvirboparai123@gmail.com>
2022-06-15 15:59:30 -0400

    added refresh route

---
+++ src/Routes/refresh.ts
@@ -0,0 +1,8 @@
+export = {};
+const express = require("express");
+const router = express.Router();
+const refreshTokenController = require("../Controllers/refreshTokenController");
+
+router.get("/", refreshTokenController.handleRefreshToken);
+
+module.exports = router;
