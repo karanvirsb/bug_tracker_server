383bb78a51c308abbcc86d2535d12c0f7755688a
mrboparai <karanvirboparai123@gmail.com>
2022-06-15 10:52:28 -0400

    checks to see if the origin is there

---
+++ src/Config/corsOptions.ts
@@ -0,0 +1,16 @@
+export = {};
+const allowedOrigins = require("./allowedOrigins");
+
+const corsOptions = {
+    origin: (origin: string, callback: Function) => {
+        // checking to see if the origin is in allowedOrigins
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not Allowed by CORS"));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
