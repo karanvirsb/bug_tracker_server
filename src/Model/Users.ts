571d95d8bbc6d819c7b8d76026fe0c8349ebe057
mrboparai <karanvirboparai123@gmail.com>
2022-06-15 15:22:09 -0400

    added functions due to cohesiveness

--- src/Model/Users.ts
+++ src/Model/Users.ts
@@ -24,4 +24,61 @@ const usersSchema = new Schema({
 
 const Users = mongoose.Model("Users", usersSchema);
 
-module.exports = { Users };
+// FUNCTIONS
+
+async function getUser(id: String): Promise<IUser | []> {
+    let user = [];
+    try {
+        // to check if its an email
+        if (id.includes("@")) {
+            user = await Users.find({ email: id }).exec();
+            if (!user) {
+                return [];
+            }
+            return user[0];
+        }
+        user = await Users.find({ username: id }).exec();
+        if (!user) {
+            return [];
+        }
+        return user[0];
+    } catch (error) {
+        return [];
+    }
+}
+
+async function saveUser(user: IUser): Promise<Boolean> {
+    try {
+        return Users.create(user) ? true : false;
+    } catch (error) {
+        return false;
+    }
+}
+
+async function updateUser(username: String, updates: Object): Promise<Boolean> {
+    try {
+        const updatedBook = await Users.updateOne(
+            { username: username },
+            updates
+        );
+        return updatedBook.acknowledged;
+    } catch (err) {
+        return false;
+    }
+}
+
+async function getUserByRefreshToken(token: String) {
+    const user = await Users.find({ refreshToken: token }).exec();
+    if (!user) {
+        return [];
+    }
+    return user[0];
+}
+
+module.exports = {
+    Users,
+    getUser,
+    saveUser,
+    getUserByRefreshToken,
+    updateUser,
+};
