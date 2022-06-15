f1fd3d7337c795a52d64bc265563f6e3d29ebc1c
mrboparai <karanvirboparai123@gmail.com>
2022-06-15 17:23:20 -0400

    added middleware type

--- src/Middleware/credentials.ts
+++ src/Middleware/credentials.ts
@@ -1,9 +1,12 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response, RequestHandler } from "express";
 
-export = {};
 const allowedOrigins = require("../Config/allowedOrigins");
 
-const credentials = (req: Request, res: Response, next: NextFunction) => {
+const credentials: RequestHandler = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     const origin = req.header("origin");
     if (allowedOrigins.includes(origin)) {
         res.header("Access-Control-Allow-Credentials", "true");
