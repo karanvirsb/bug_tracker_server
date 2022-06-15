cbab6bff8efa547f8397880dc84bdd273cddd3ed
mrboparai <karanvirboparai123@gmail.com>
2022-06-14 22:07:04 -0400

    created a server file

--- server.ts
+++ server.ts
@@ -1,11 +1,21 @@
+require("dotenv");
 const express = require("express");
 const app = express();
 const port = 8000;
+const dbController = require("./src/Controllers/databaseController");
+const mongoose = require("mongoose");
 
-app.get("/", (req: Request, res: Response) => {
-    console.log("/ request");
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+dbController.connect();
+mongoose.connection.once("open", () => {
+    console.log("open");
+    app.listen(port, () => {
+        console.log("Listening on port " + port);
+    });
 
-app.listen(port, () => {
-    console.log("Listening on port " + port);
+    app.on("beforeExit", () => {
+        console.log("beforeExit");
+        dbController.disconnect();
+    });
 });
