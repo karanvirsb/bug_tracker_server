55e1e06ed44d10d73810c60c319d529706bc9d98
mrboparai <karanvirboparai123@gmail.com>
2022-06-15 15:58:53 -0400

    created refreshTokenController

---
+++ src/Controllers/refreshTokenController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import { DecodeOptions, JwtPayload, VerifyErrors } from "jsonwebtoken";
+
+const jwt = require("jsonwebtoken");
+const { Users } = require("./databaseController");
+
+const handleRefreshToken = async (req: Request, res: Response) => {
+    // look for the cookie with the jwt
+    const cookies = req.cookies;
+
+    if (!cookies.jwt) return res.sendStatus(401); // unauthorized
+
+    const refreshToken = cookies.jwt;
+
+    const foundUser = await Users.getUserByRefreshToken(refreshToken);
+
+    if (!foundUser) return res.sendStatus(403);
+
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err: VerifyErrors, decoded: JwtPayload) => {
+            if (err || foundUser.username !== decoded.username)
+                return res.sendStatus(403);
+
+            const roles = Object.values(foundUser.roles);
+            const group_id = Object.values(foundUser.group_id);
+
+            // new access token
+            const accessToken = jwt.sign(
+                {
+                    UserInfo: {
+                        username: decoded.username,
+                        roles: roles,
+                        group_id: group_id,
+                    },
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "30m" }
+            );
+
+            res.json({ accessToken });
+        }
+    );
+};
+
+module.exports = { handleRefreshToken };
