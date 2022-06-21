5536717ea05d8d1b99c56be4e37be9ff7facdab6
mrboparai <karanvirboparai123@gmail.com>
2022-06-21 16:37:56 -0400

    removed space

--- server.ts
+++ server.ts
@@ -19,11 +19,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 // ROUTES
-
 app.use("/register", require("./src/Routes/register"));
 app.use("/login", require("./src/Routes/login"));
 app.use("/refresh", require("./src/Routes/refresh"));
 app.use("/logout", require("./src/Routes/logout"));
+
 // Protected routes
 app.use(verifyJWT);
 
