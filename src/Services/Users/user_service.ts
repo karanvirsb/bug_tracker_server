1d43c3430ea78a04a0787124ac4fac40dea8d033
mrboparai <karanvirboparai123@gmail.com>
2022-06-22 13:59:01 -0400

    created service that contains functions for user

---
+++ src/Services/Users/user_service.ts
@@ -0,0 +1,62 @@
+import { IUser } from "../../Model/Users";
+
+// GETS
+const getUser =
+    (User: any) =>
+    async (id: String): Promise<{} | null> => {
+        if (!id) throw Error("id was not provided");
+
+        if (id.includes("@")) {
+            return await User.find({ email: id }).exec();
+        }
+
+        return await User.find({ username: id }).exec();
+    };
+
+const getUserByRefreshToken = (User: any) => async (token: String) => {
+    if (!User) throw Error("User data was not provided");
+    const user = await User.find({ refreshToken: token }).exec();
+    return user;
+};
+
+// CREATES
+const saveUser = (User: any) => async (user: IUser) => {
+    if (!User) throw Error("User data was not provided");
+    const createdUser = new User(user);
+    return await createdUser.save();
+};
+
+// UPDATES
+const updateUser =
+    (User: any) =>
+    async (username: String, updates: Object): Promise<Boolean | null> => {
+        if (!username) throw Error("No username was provided");
+        if (!updates) throw Error("No updates were provided");
+
+        const updatedBook = await User.updateOne(
+            { username: username },
+            updates
+        );
+        return updatedBook.acknowledged;
+    };
+
+// Delete user
+
+const deleteUser =
+    (User: any) =>
+    async (id: String): Promise<Boolean | null> => {
+        if (!id) throw Error("No username was passed");
+        const deletedUser = await User.deleteOne({ username: id });
+        return deletedUser.acknowledged;
+    };
+
+// the User here represents the User model
+module.exports = (User: any) => {
+    return {
+        createUser: saveUser(User),
+        getUser: getUser(User),
+        getUserByRefreshToken: getUserByRefreshToken(User),
+        updateUser: updateUser(User),
+        deleteUser: deleteUser(User),
+    };
+};
