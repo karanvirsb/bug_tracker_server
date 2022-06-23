b8ec3cd79ffe86cf4ae4de22a0933e84d9e13997
mrboparai <karanvirboparai123@gmail.com>
2022-06-23 12:55:33 -0400

    added update test

---
+++ src/Services/Users/user_service.test.ts
@@ -0,0 +1,70 @@
+const UserService = require("./user_service");
+const Users = require("../../Model/Users");
+import { IUser } from "../../Model/Users";
+const sinon = require("sinon");
+export {};
+const mongoose = require("mongoose");
+const mongodb = "mongodb://localhost:27017/bugTracker_serviceTest";
+mongoose.connect(mongodb);
+
+describe("UserService tests", () => {
+    beforeAll(async () => {
+        await Users.remove({});
+    });
+
+    afterAll(async () => {
+        await Users.remove({});
+        await mongoose.disconnect();
+    });
+
+    const userData: [IUser] = [
+        {
+            username: "John21",
+            password: "John_123",
+            email: "John1@gmail",
+            firstName: "John",
+            lastName: "Smith",
+        },
+    ];
+
+    it("has a module", () => {
+        expect(Users).toBeDefined();
+    });
+
+    test("create user", async () => {
+        const userService = UserService(Users);
+        const createdUser = await userService.createUser(userData[0]);
+        const expectedUser = userData[0];
+        const actualUser = createdUser;
+
+        expect(actualUser).toMatchObject(expectedUser);
+    });
+
+    test("update user", async () => {
+        const userService = UserService(Users);
+        const updatedUser = await userService.updateUser(userData[0].username, {
+            firstName: "Johnny",
+        });
+
+        expect(updatedUser).toBe(true);
+    });
+
+    // test("Create user", async () => {
+    //     const save = sinon.spy();
+    //     let user;
+
+    //     const MockModel = function (data) {
+    //         user = data;
+    //         return { user: data, save };
+    //     };
+
+    //     const userService = UserService(MockModel);
+
+    //     await userService.createUser(userData.user);
+
+    //     const expected = true;
+    //     const actual = save.calledOnce;
+    //     expect(actual).toBe(expected);
+    //     expect(user).toBe(userData.user);
+    // });
+});
