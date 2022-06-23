d0f2999271d78e13f48ba92a02bf7b98f4c5bc70
mrboparai <karanvirboparai123@gmail.com>
2022-06-22 21:24:50 -0400

    renamed projects

---
+++ src/Model/Projects/index.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+export interface IProject {
+    projectId: String;
+    groupId: String;
+    projectName: String;
+    projectDesc: String;
+    dateCreated?: Date;
+    users?: [];
+}
+
+const projectSchema = new Schema<IProject>({
+    projectId: { type: String, unique: true },
+    groupId: { type: String },
+    projectName: { type: String },
+    projectDesc: { type: String },
+    dateCreated: { type: Date, default: Date.now },
+    users: { type: [] },
+});
+
+module.exports = model<IProject>("Projects", projectSchema);
