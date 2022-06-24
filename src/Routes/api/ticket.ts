9d6dc7d9362f95a5257ccf179c8315b23af56349
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 18:53:32 -0400

    created routes for tickets

---
+++ src/Routes/api/ticket.ts
@@ -0,0 +1,25 @@
+export {};
+
+const express = require("express");
+const router = express.Router();
+
+const {
+    createTicket,
+    deleteTicket,
+    updateTicket,
+    getTicket,
+    assignUserToTicket,
+    removeUserFromTicket,
+    getStatistics,
+} = require("../../Controllers/ticketController");
+
+router
+    .route("/")
+    .post(createTicket)
+    .put(updateTicket)
+    .delete(deleteTicket)
+    .get(getStatistics);
+router.route("/:id").get(getTicket);
+router.route("/user").post(assignUserToTicket).delete(removeUserFromTicket);
+
+module.exports = router;
