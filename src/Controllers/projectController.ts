47b30daa20fbb02764488ed124bf53701af84f70
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 16:21:50 -0400

    created outline of project controller

---
+++ src/Controllers/projectController.ts
@@ -0,0 +1,23 @@
+export {};
+import { NextFunction, Request, Response } from "express";
+const ProjectService = require("../Services/Projects");
+
+const createProject = () => {};
+const getProject = () => {};
+const updateProject = () => {};
+const deleteProject = () => {};
+const addUserToProject = () => {};
+const removeUserFromProject = () => {};
+const getAllProjectsByGroupId = () => {};
+const getAllUsersOfProject = () => {};
+
+module.exports = {
+    createProject,
+    getProject,
+    updateProject,
+    deleteProject,
+    addUserToProject,
+    removeUserFromProject,
+    getAllProjectsByGroupId,
+    getAllUsersOfProject,
+};
