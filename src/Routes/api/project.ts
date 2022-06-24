57aa2ad739decdbb5705a7b14edc22e2f9acef5e
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 16:21:39 -0400

    created routes for project

---
+++ src/Routes/api/project.ts
@@ -0,0 +1,23 @@
+export {};
+
+const express = require("express");
+const router = express.Router();
+
+const {
+    createProject,
+    getProject,
+    updateProject,
+    deleteProject,
+    addUserToProject,
+    removeUserFromProject,
+    getAllProjectsByGroupId,
+    getAllUsersOfProject,
+} = require("../../Controllers/projectController");
+
+router.route("/").post(createProject).put(updateProject).delete(deleteProject);
+router.route("/:id").get(getProject);
+router.route("/group/:id").get(getAllProjectsByGroupId);
+router.route("/user").post(addUserToProject).delete(removeUserFromProject);
+router.route("/user/:id").get(getAllUsersOfProject); // TODO questionable need to rethink if this is needed
+
+module.exports = router;
