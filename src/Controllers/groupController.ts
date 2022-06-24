ac9665081fe80b1e583bae3b2f47855dadf4f906
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 11:38:47 -0400

    created function that creates the group

--- src/Controllers/groupController.ts
+++ src/Controllers/groupController.ts
@@ -1,9 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+
 export {};
-const GroupService = require('../Services/Groups');
+const GroupService = require("../Services/Groups");
 
-const createGroup = () => {};
-const getGroup = () = {};
-const updateGroup = () = {};
-const deleteGroup = () = {};
+const createGroup = async (req: Request, res: Response, next: NextFunction) => {
+    const { groupId, groupName } = req.body;
+    try {
+        const createdGroup = await GroupService.createGroup({
+            groupId,
+            groupName,
+        });
+        if (createdGroup) return res.sendStatus(201);
+        return res.sendStatus(502);
+    } catch (error) {
+        next(error);
+    }
+};
+const getGroup = () => {};
+const updateGroup = () => {};
+const deleteGroup = () => {};
 
-module.exports = {createGroup, getGroup, updateGroup, deleteGroup};
+module.exports = { createGroup, getGroup, updateGroup, deleteGroup };
