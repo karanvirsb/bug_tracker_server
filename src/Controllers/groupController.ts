2197192b17dc9714a366a0e7b57413620e2694a2
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 11:26:46 -0400

    added group template for controller

---
+++ src/Controllers/groupController.ts
@@ -0,0 +1,9 @@
+export {};
+const GroupService = require('../Services/Groups');
+
+const createGroup = () => {};
+const getGroup = () = {};
+const updateGroup = () = {};
+const deleteGroup = () = {};
+
+module.exports = {createGroup, getGroup, updateGroup, deleteGroup};
