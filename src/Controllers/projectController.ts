d815b07fdfce6ecbadd2128ef86e134080477120
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 16:37:13 -0400

    implemented function for creating a project

--- src/Controllers/projectController.ts
+++ src/Controllers/projectController.ts
@@ -2,7 +2,23 @@ export {};
 import { NextFunction, Request, Response } from "express";
 const ProjectService = require("../Services/Projects");
 
-const createProject = () => {};
+const createProject = (req: Request, res: Response, next: NextFunction) => {
+    const { projectId, groupId, projectName, projectDesc, users } = req.body;
+    try {
+        const createdProject = ProjectService.createProject({
+            projectId,
+            groupId,
+            projectName,
+            projectDesc,
+            users,
+        });
+
+        if (createdProject) return res.sendStatus(200);
+        return res.sendStatus(502);
+    } catch (error) {
+        next(error);
+    }
+};
 const getProject = () => {};
 const updateProject = () => {};
 const deleteProject = () => {};
