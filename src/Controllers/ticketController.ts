3f8485ece32a17e8e1553308c71456c4b3be8afb
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 19:32:40 -0400

    implemented create ticket function

--- src/Controllers/ticketController.ts
+++ src/Controllers/ticketController.ts
@@ -1,8 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+
 export {};
 
 const TicketService = require("../Services/Tickets");
 
-const createTicket = async () => {};
+const createTicket = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const {
+        ticketId,
+        dateCreated,
+        title,
+        description,
+        assignedDev = [],
+        time,
+        ticketStatus,
+        ticketSeverity,
+        ticketType,
+        reporterId,
+        projectId,
+    } = req.body;
+    try {
+        const createdTicket = await TicketService.createTicket({
+            ticketId,
+            dateCreated,
+            title,
+            description,
+            assignedDev,
+            time,
+            ticketStatus,
+            ticketSeverity,
+            ticketType,
+            reporterId,
+            projectId,
+        });
+        if (createdTicket) return res.sendStatus(200);
+        return res.sendStatus(502);
+    } catch (error) {
+        next(error);
+    }
+};
 const deleteTicket = async () => {};
 const updateTicket = async () => {};
 const getTicket = async () => {};
