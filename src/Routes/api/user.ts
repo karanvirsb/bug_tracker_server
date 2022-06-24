6cd42a70f1b28c1a7524740580c828ad16848ae1
mrboparai <karanvirboparai123@gmail.com>
2022-06-23 22:34:56 -0400

    added multiple routes

--- src/Routes/api/user.ts
+++ src/Routes/api/user.ts
@@ -9,4 +9,8 @@ const {
     updateUser,
 } = require("../../Controllers/User");
 
-router.route("/id").post(getUser);
+router.route("/").delete(deleteUser).post(createUser);
+router.route("/id").post(getUser).put(updateUser);
+router.route("/token").post(getUserByRefreshToken);
+
+module.exports = router;
