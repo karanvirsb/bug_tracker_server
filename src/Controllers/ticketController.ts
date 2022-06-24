e33ce44744579d8b38b6c396e4e404a02fc25295
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 18:53:48 -0400

    addd ticket template

---
+++ src/Controllers/ticketController.ts
@@ -0,0 +1,21 @@
+export {};
+
+const TicketService = require("../Services/Tickets");
+
+const createTicket = async () => {};
+const deleteTicket = async () => {};
+const updateTicket = async () => {};
+const getTicket = async () => {};
+const assignUserToTicket = async () => {};
+const removeUserFromTicket = async () => {};
+const getStatistics = async () => {};
+
+module.exports = {
+    createTicket,
+    deleteTicket,
+    updateTicket,
+    getTicket,
+    assignUserToTicket,
+    removeUserFromTicket,
+    getStatistics,
+};
