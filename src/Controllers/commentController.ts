04945e71fea7125200f045382d29a57466bc9ffd
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 20:03:29 -0400

    created template

---
+++ src/Controllers/commentController.ts
@@ -0,0 +1,20 @@
+export {};
+const CommentService = require("../Services/Comments");
+
+const createComment = async () => {};
+const deleteComment = async () => {};
+const updateComment = async () => {};
+const getComment = async () => {};
+const replyTo = async () => {};
+const getReplyIds = async () => {};
+const getAllComments = async () => {};
+
+module.exports = {
+    createComment,
+    deleteComment,
+    updateComment,
+    getComment,
+    replyTo,
+    getReplyIds,
+    getAllComments,
+};
