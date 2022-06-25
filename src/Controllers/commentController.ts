81d1dd8a23f50b9c888f726be4fb660fc53a1d65
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 20:55:18 -0400

    implemented update functionality

--- src/Controllers/commentController.ts
+++ src/Controllers/commentController.ts
@@ -23,8 +23,43 @@ const createComment = async (
         next(error);
     }
 };
-const deleteComment = async () => {};
-const updateComment = async () => {};
+const deleteComment = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { commentId } = req.body;
+
+    try {
+        const deletedComment = await CommentService.deleteComment(commentId);
+
+        if (deletedComment) return res.sendStatus(200);
+        res.sendStatus(502);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateComment = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { commentId, updates } = req.body;
+
+    try {
+        const updatedComment = await CommentService.updateComment(
+            commentId,
+            updates
+        );
+
+        if (updatedComment) return res.sendStatus(200);
+        res.sendStatus(502);
+    } catch (error) {
+        next(error);
+    }
+};
+
 const getComment = async () => {};
 const replyTo = async () => {};
 
