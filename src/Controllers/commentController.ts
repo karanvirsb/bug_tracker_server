201787df15b597ed2f6c88cf82350a5478dbf22b
mrboparai <karanvirboparai123@gmail.com>
2022-06-24 20:35:03 -0400

    implemented createComment function

--- src/Controllers/commentController.ts
+++ src/Controllers/commentController.ts
@@ -1,11 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+
 export {};
 const CommentService = require("../Services/Comments");
 
-const createComment = async () => {};
+const createComment = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { commentId, userId, ticketId, comment } = req.body;
+
+    try {
+        const createdComment = await CommentService.createComment({
+            commentId,
+            userId,
+            ticketId,
+            comment,
+        });
+        if (createdComment) return res.sendStatus(200);
+        res.sendStatus(502);
+    } catch (error) {
+        next(error);
+    }
+};
 const deleteComment = async () => {};
 const updateComment = async () => {};
 const getComment = async () => {};
 const replyTo = async () => {};
+
+// questionable may not be needed
 const getReplyIds = async () => {};
 const getAllComments = async () => {};
 
