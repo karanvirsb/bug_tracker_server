e2c034c726e4d7755c708d64aef1e8cbf65ec53a
mrboparai <karanvirboparai123@gmail.com>
2022-06-27 08:22:02 -0400

    created an index for all models

---
+++ src/Model/index.ts
@@ -0,0 +1,7 @@
+const Users = require("./Users");
+const Groups = require("./Groups");
+const Projects = require("./Projects");
+const Tickets = require("./Tickets");
+const Comments = require("./Comments");
+
+export { Users, Groups, Projects, Tickets, Comments };
