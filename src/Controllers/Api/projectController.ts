a7d91d23a9175c292d4fdd721edbac104b0a7715
mrboparai <karanvirboparai123@gmail.com>
2022-06-27 14:15:05 -0400

    handled error when update project does not exist and when property doesnt exist

--- src/Controllers/Api/projectController.ts
+++ src/Controllers/Api/projectController.ts
@@ -55,10 +55,22 @@ const updateProject = async (
     next: NextFunction
 ) => {
     const { id, updates } = req.body;
+    if (!id) throw Error("Invalid Id");
+    const updatesKeys = Object.keys(updates);
+
+    for (let i = 0; i < updatesKeys.length; i++) {
+        if (!IProject._getCached().keys.includes(updatesKeys[i]))
+            return res
+                .status(400)
+                .json({
+                    message: `Update property ${updatesKeys[i]} does not exist`,
+                });
+    }
+
     try {
         const updatedProject = await ProjectService.updateProject(id, updates);
         if (updatedProject) return res.sendStatus(200);
-        return res.sendStatus(502);
+        return res.sendStatus(204);
     } catch (error) {
         next(error);
     }
