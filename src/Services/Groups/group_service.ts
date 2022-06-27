5ebc90dfeb4810681d619d5d79e6739c26842818
mrboparai <karanvirboparai123@gmail.com>
2022-06-27 10:00:20 -0400

    updated group service with groupType

--- src/Services/Groups/group_service.ts
+++ src/Services/Groups/group_service.ts
@@ -1,29 +1,30 @@
-import { IGroup } from "../../Model/Groups";
+import { groupType } from "../../Model/Groups";
+import {Model} from 'mongoose';
 
 const createGroup =
-    (Groups: any) =>
-    async (groupInfo: IGroup): Promise<IGroup> => {
+    (Groups: typeof Model<groupType>) =>
+    async (groupInfo: groupType): Promise<groupType> => {
         const group = new Groups(groupInfo);
         return await group.save();
     };
 
-const updateGroup = (Groups: any) => async (groupId: String, updates: {}) => {
+const updateGroup = (Groups: typeof Model<groupType>) => async (groupId: String, updates: {}) => {
     const updatedGroup = await Groups.updateOne({ groupId: groupId }, updates);
     return updatedGroup.acknowledged;
 };
 
-const deleteGroup = (Groups: any) => async (groupId: String) => {
+const deleteGroup = (Groups: typeof Model<groupType>) => async (groupId: String) => {
     const deletedGroup = await Groups.deleteOne({
         groupId: groupId,
     }).exec();
     return deletedGroup.acknowledged;
 };
 
-const getGroup = (Groups: any) => async (groupId: String) => {
+const getGroup = (Groups: typeof Model<groupType>) => async (groupId: String) => {
     return await Groups.findOne({ groupId: groupId }).exec();
 };
 
-module.exports = (Group: any) => {
+module.exports = (Group: typeof Model<groupType>) => {
     return {
         createGroup: createGroup(Group),
         deleteGroup: deleteGroup(Group),
