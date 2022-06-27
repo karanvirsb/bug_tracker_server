ddca12af84f0590a19d50a1c2904f7b754df9e10
mrboparai <karanvirboparai123@gmail.com>
2022-06-27 14:40:18 -0400

    fixed import needed brackets

--- src/Model/index.ts
+++ src/Model/index.ts
@@ -1,7 +1,7 @@
 import { Users } from "./Users";
 import { Groups } from "./Groups";
 import { Projects } from "./Projects";
-import Tickets from "./Tickets";
+import { Tickets } from "./Tickets";
 const Comments = require("./Comments");
 
 export { Users, Groups, Projects, Tickets, Comments };
