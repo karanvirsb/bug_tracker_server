a1aa58623f8107f0bc6154df9d1e080181ff548b
mrboparai <karanvirboparai123@gmail.com>
2022-06-28 18:09:01 -0400

    created socket

---
+++ sockets.ts
@@ -0,0 +1,16 @@
+import { Server } from "socket.io";
+const allowedOrigins = require("./src/Config/allowedOrigins");
+
+const socketListen = (app: any) => {
+    const io = new Server(app, {
+        cors: { origin: allowedOrigins, credentials: true },
+    });
+
+    console.log("connection");
+
+    io.on("connection", (socket) => {
+        console.log(socket);
+    });
+};
+
+module.exports = { socketListen };
