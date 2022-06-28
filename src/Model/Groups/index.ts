b0fba3e0a2f4ea008a970562030350affe9e51cd
mrboparai <karanvirboparai123@gmail.com>
2022-06-27 22:04:45 -0400

    updated groupSchema to ahve invite code

--- src/Model/Groups/index.ts
+++ src/Model/Groups/index.ts
@@ -9,6 +9,7 @@ import { z } from "zod";
 const IGroup = z.object({
     groupId: z.string().min(1),
     groupName: z.string().min(4).max(50),
+    groupInviteCode: z.string().min(8),
     dateCreated: z.date().optional(),
 });
 
@@ -16,7 +17,8 @@ export type groupType = z.infer<typeof IGroup>;
 
 const groupSchema = new Schema<groupType>({
     groupId: { type: String, unique: true },
-    groupName: { type: String, unique: true },
+    groupName: { type: String },
+    groupInviteCode: { type: String, unique: true },
     dateCreated: { type: Date, default: Date.now },
 });
 
