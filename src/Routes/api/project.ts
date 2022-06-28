8f4a8b6b33d9d2aa98eab2e9c86065b17a44b568
mrboparai <karanvirboparai123@gmail.com>
2022-06-28 09:37:25 -0400

    updated get id route to post

--- src/Routes/api/project.ts
+++ src/Routes/api/project.ts
@@ -15,7 +15,7 @@ const {
 } = require("../../Controllers/Api/projectController");
 
 router.route("/").post(createProject).put(updateProject).delete(deleteProject);
-router.route("/:id").get(getProject);
+router.route("/id").post(getProject);
 router.route("/group/:id").get(getAllProjectsByGroupId);
 router.route("/user").post(addUserToProject).delete(removeUserFromProject);
 router.route("/user/:id").get(getAllUsersOfProject); // TODO questionable need to rethink if this is needed
