7b04038c481872987b6fef26c75079e0469add3b
mrboparai <karanvirboparai123@gmail.com>
2022-06-28 18:07:52 -0400

    added socket

--- server.ts
+++ server.ts
@@ -3,16 +3,20 @@ const port = 8000;
 const dbController = require("./src/Controllers/databaseController");
 const mongoose = require("mongoose");
 const app = require("./app");
+const { createServer } = require("http");
+const httpServer = createServer(app);
+const { socketListen } = require("./sockets");
 
 dbController.connect();
 
 mongoose.connection.once("open", () => {
-    console.log("open");
-    app.listen(port, () => {
+    socketListen(httpServer);
+
+    httpServer.listen(port, () => {
         console.log("Listening on port " + port);
     });
 
-    app.on("beforeExit", () => {
+    httpServer.on("beforeExit", () => {
         console.log("beforeExit");
         dbController.disconnect();
     });
