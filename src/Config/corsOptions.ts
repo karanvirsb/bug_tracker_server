cdfa44c00f1f4288e7721fa5f64e3461a1fd663e
mrboparai <karanvirboparai123@gmail.com>
2022-06-29 16:41:49 -0400

    updated import for allowed origins

--- src/Config/corsOptions.ts
+++ src/Config/corsOptions.ts
@@ -1,7 +1,7 @@
-const allowedOrigins = require("./allowedOrigins");
+import allowedOrigins from "./allowedOrigins";
 
 const corsOptions = {
-    origin: (origin: string | undefined, callback: Function) => {
+    origin: (origin: string, callback: Function) => {
         // checking to see if the origin is in allowedOrigins
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
