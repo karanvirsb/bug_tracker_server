4c116d5076fb6927e3ee0e193fe38921fb7a70d0
mrboparai <karanvirboparai123@gmail.com>
2022-06-28 22:05:39 -0400

    added an index for routes

---
+++ src/Routes/index.ts
@@ -0,0 +1,21 @@
+import registerRouter from "./register";
+import loginRouter from "./login";
+import refreshRouter from "./refresh";
+import logoutRouter from "./logout";
+import userRouter from "./api/user";
+import groupRouter from "./api/group";
+import projectRouter from "./api/project";
+import ticketRouter from "./api/ticket";
+import commentRouter from "./api/comment";
+
+export {
+    registerRouter,
+    loginRouter,
+    refreshRouter,
+    logoutRouter,
+    userRouter,
+    groupRouter,
+    projectRouter,
+    ticketRouter,
+    commentRouter,
+};
