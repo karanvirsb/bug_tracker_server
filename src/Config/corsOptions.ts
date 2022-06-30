9e14a282d551e8095f28eddb49c27668fb076564
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 11:18:59 -0400

    changed cors to apply to type

--- src/Config/corsOptions.ts
+++ src/Config/corsOptions.ts
@@ -1,15 +1,9 @@
+import cors from "cors";
 import allowedOrigins from "./allowedOrigins";
 
-const corsOptions = {
-    origin: (origin: string, callback: Function) => {
-        // checking to see if the origin is in allowedOrigins
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not Allowed by CORS"));
-        }
-    },
-    optionsSuccessStatus: 200,
+const corsOptions: cors.CorsOptions = {
+    origin: allowedOrigins,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
 };
 
 export default corsOptions;
