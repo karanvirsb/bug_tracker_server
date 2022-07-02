edaa29cf40ad280b7a78cec3534bd0d49b612669
mrboparai <karanvirboparai123@gmail.com>
2022-07-01 22:14:50 -0400

    added check for development

--- src/Controllers/databaseController.ts
+++ src/Controllers/databaseController.ts
@@ -1,14 +1,26 @@
 import mongoose from "mongoose";
 
 function connect(): void {
-    mongoose
-        .connect(process?.env?.DATABASE_URL ?? "")
-        .then(() => {
-            console.log("running");
-        })
-        .catch((err: Error) => {
-            console.log(err);
-        });
+    console.log(process.env.NODE_ENV);
+    if (process.env.NODE_ENV === undefined) {
+        mongoose
+            .connect("mongodb://localhost:27017/bugTracker_development")
+            .then(() => {
+                console.log("running");
+            })
+            .catch((err: Error) => {
+                console.log(err);
+            });
+    } else {
+        mongoose
+            .connect(process?.env?.DATABASE_URL ?? "")
+            .then(() => {
+                console.log("running");
+            })
+            .catch((err: Error) => {
+                console.log(err);
+            });
+    }
 }
 
 function disconnect(): void {
