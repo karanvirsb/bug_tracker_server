d3eadb8a55131ca2b03a37669f5f95b6354236ff
mrboparai <karanvirboparai123@gmail.com>
2022-07-04 11:24:32 -0400

    added on joinRoom event

--- sockets.ts
+++ sockets.ts
@@ -5,6 +5,16 @@ const socketListen = (app: any) => {
     io = new Server(app, {
         cors: { origin: "http://localhost:3000" },
     });
+
+    io.sockets.on("connection", (socket) => {
+        socket.on("joinRoom", (roomId) => {
+            console.log(roomId);
+            if (!roomId) {
+                socket.emit("error", { message: "Room id was not given" });
+            }
+            socket.join(roomId);
+        });
+    });
 };
 
 const wrap = (middleware: any) => (socket: any, next: any) => {
