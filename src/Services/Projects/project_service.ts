5b34f58d44a6eb4aa65f2917fb6d51cd3b228268
mrboparai <karanvirboparai123@gmail.com>
2022-07-07 09:40:58 -0400

    updated all types to support paginate and updated create

--- src/Services/Projects/project_service.ts
+++ src/Services/Projects/project_service.ts
@@ -1,15 +1,17 @@
+import mongoose from "mongoose";
 import { Model } from "mongoose";
 import { projectType } from "../../Model/Projects";
 
 const createProject =
-    (Projects: typeof Model<projectType>) =>
+    (Projects: mongoose.PaginateModel<projectType>) =>
     async (projectInfo: projectType) => {
-        const project = new Projects(projectInfo);
-        return await project.save();
+        const project = await Projects.create(projectInfo);
+        return project;
     };
 
 const updateProject =
-    (Projects: typeof Model<projectType>) => async (projectId: String, updates: {}) => {
+    (Projects: mongoose.PaginateModel<projectType>) =>
+    async (projectId: String, updates: {}) => {
         const updatedProject = await Projects.updateOne(
             { projectId: projectId },
             updates
@@ -18,27 +20,33 @@ const updateProject =
         return updatedProject.acknowledged && updatedProject.matchedCount === 1;
     };
 
-const deleteProject = (Projects: typeof Model<projectType>) => async (projectId: String) => {
-    const deletedProject = await Projects.deleteOne({
-        projectId: projectId,
-    }).exec();
-    // checking to see if it was deleted and atleast 1 got deleted
-    return deletedProject.acknowledged && deletedProject.deletedCount === 1;
-};
+const deleteProject =
+    (Projects: mongoose.PaginateModel<projectType>) =>
+    async (projectId: String) => {
+        const deletedProject = await Projects.deleteOne({
+            projectId: projectId,
+        }).exec();
+        // checking to see if it was deleted and atleast 1 got deleted
+        return deletedProject.acknowledged && deletedProject.deletedCount === 1;
+    };
 
 const getProject =
-    (Projects: typeof Model<projectType>) =>
-    async (projectInfo: { filter: "projectId" | "projectName", val: String }) => {
+    (Projects: mongoose.PaginateModel<projectType>) =>
+    async (projectInfo: {
+        filter: "projectId" | "projectName";
+        val: String;
+    }) => {
         return await Projects.findOne({
             [projectInfo.filter]: projectInfo.val,
         }).exec();
     };
 
 const addUserToProject =
-    (Projects: typeof Model<projectType>) => async (projectId: String, userId: String) => {
+    (Projects: mongoose.PaginateModel<projectType>) =>
+    async (projectId: String, userId: String) => {
         /*
-        * add a new user to a project 
-        */
+         * add a new user to a project
+         */
         const project = await Projects.findOne({
             projectId: projectId,
         }).exec();
@@ -53,10 +61,11 @@ const addUserToProject =
     };
 
 const removeUserFromProject =
-    (Projects: typeof Model<projectType>) => async (projectId: String, userId: String) => {
-         /*
-        * add a new user to a project 
-        */
+    (Projects: mongoose.PaginateModel<projectType>) =>
+    async (projectId: String, userId: String) => {
+        /*
+         * add a new user to a project
+         */
         const project = await Projects.findOne({
             projectId: projectId,
         }).exec();
@@ -71,17 +80,28 @@ const removeUserFromProject =
         return updatedProject.acknowledged;
     };
 
-const getAllProjectsByGroupId = (Projects: typeof Model<projectType>) => async (groupId: String) => {
-    const projects = await Projects.find({ groupId: groupId }).exec();
-    return projects;
-};
+const getAllProjectsByGroupId =
+    (Projects: mongoose.PaginateModel<projectType>) =>
+    async (groupId: String, pageNumber: number) => {
+        const projects = await Projects.paginate(
+            { groupId: groupId },
+            { limit: 5, page: pageNumber }
+        );
+        // const projects = await Projects.find({ groupId: groupId }).exec();
+        return projects;
+    };
 
-const getAllUsersOfProject = (Projects: typeof Model<projectType>) => async (projectId: String) => {
-    const users = await Projects.find({ projectId: projectId }, `users`).exec();
-    return users;
-};
+const getAllUsersOfProject =
+    (Projects: mongoose.PaginateModel<projectType>) =>
+    async (projectId: String) => {
+        const users = await Projects.find(
+            { projectId: projectId },
+            `users`
+        ).exec();
+        return users;
+    };
 
-export default (Project: typeof Model<projectType>) => {
+export default (Project: mongoose.PaginateModel<projectType>) => {
     return {
         createProject: createProject(Project),
         deleteProject: deleteProject(Project),
