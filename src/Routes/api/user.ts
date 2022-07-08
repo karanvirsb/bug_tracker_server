2bfe4ad085dc236e454b5ca61731b5c6e585dba3
mrboparai <karanvirboparai123@gmail.com>
2022-07-08 18:03:11 -0400

    updated route

--- src/Routes/api/user.ts
+++ src/Routes/api/user.ts
@@ -8,11 +8,13 @@ import {
     deleteUser,
     updateUser,
     getAllUsers,
+    getUsersByGroupId,
 } from "../../Controllers/Api/userController";
 
 router.route("/").post(createUser);
 router.route("/id").post(getUser).put(updateUser).delete(deleteUser);
 router.route("/token").post(getUserByRefreshToken);
 router.route("/users").post(getAllUsers);
+router.route("/group").post(getUsersByGroupId);
 
 export default router;
