9d46aa713ebbeb18415507bbf4651ffb053f3032
mrboparai <karanvirboparai123@gmail.com>
2022-07-10 13:17:27 -0400

    added methods and connection

--- sockets.ts
+++ sockets.ts
@@ -3,19 +3,30 @@ import { DefaultEventsMap } from "socket.io/dist/typed-events";
 let io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
 const socketListen = (app: any) => {
     io = new Server(app, {
-        cors: { origin: "http://localhost:3000" },
+        cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
     });
 
-    io.sockets.on("connection", (socket) => {
+    io.on("connection", (socket) => {
+        socket.emit("connection", socket.id);
         socket.on("joinRoom", (roomId) => {
             console.log(
-                "🚀 ~ file: sockets.ts ~ line 11 ~ socket.on ~ roomId",
+                "🚀 ~ file: sockets.ts ~ line 12 ~ socket.on ~ roomId",
                 roomId
             );
+
             if (!roomId) {
                 socket.emit("error", { message: "Room id was not given" });
             }
             socket.join(roomId);
+        });
+
+        // invalidating query for all users
+        socket.on("invalidateQuery", (data) => {
+            console.log(
+                "🚀 ~ file: sockets.ts ~ line 19 ~ socket.on ~ data",
+                data
+            );
+            io.sockets.to(data.groupId).emit("invalidateData", data.queryName);
         });
     });
 };
