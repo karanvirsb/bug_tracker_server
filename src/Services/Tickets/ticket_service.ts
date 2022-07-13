c42c3445b1c8914b2dd2b4236c72dd904f49fa20
mrboparai <karanvirboparai123@gmail.com>
2022-07-13 11:16:10 -0400

    updated type to include paginate and add function to get tickets by id

--- src/Services/Tickets/ticket_service.ts
+++ src/Services/Tickets/ticket_service.ts
@@ -1,15 +1,17 @@
-import { Model } from "mongoose";
+import mongoose from "mongoose";
+import { number } from "zod";
 import { ticketType } from "../../Model/Tickets";
 
 const createTicket =
-    (Tickets: typeof Model <ticketType>) =>
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
     async (ticketInfo: ticketType) => {
         const ticket = new Tickets(ticketInfo);
         return await ticket.save();
     };
 
 const updateTicket =
-    (Tickets: typeof Model <ticketType>) => async (ticketId: String, updates: {}) => {
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
+    async (ticketId: String, updates: {}) => {
         const updatedTicket = await Tickets.updateOne(
             { ticketId: ticketId },
             updates
@@ -18,30 +20,55 @@ const updateTicket =
         return updatedTicket.acknowledged && updatedTicket.matchedCount === 1;
     };
 
-const deleteTicket = (Tickets: typeof Model <ticketType>) => async (ticketId: String) => {
-    const deletedTicket = await Tickets.deleteOne({
-        ticketId: ticketId,
-    }).exec();
-    // checking to see if it was deleted and atleast 1 got deleted
-    return deletedTicket.acknowledged && deletedTicket.deletedCount === 1;
-};
+const deleteTicket =
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
+    async (ticketId: String) => {
+        const deletedTicket = await Tickets.deleteOne({
+            ticketId: ticketId,
+        }).exec();
+        // checking to see if it was deleted and atleast 1 got deleted
+        return deletedTicket.acknowledged && deletedTicket.deletedCount === 1;
+    };
 
 const getTicket =
-    (Tickets: typeof Model<ticketType>) =>
-    async (ticketInfo: { filter: "ticketId" | "reporterId" | "projectId"; val: string }) => {
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
+    async (ticketInfo: {
+        filter: "ticketId" | "reporterId" | "projectId";
+        val: string;
+    }) => {
         return await Tickets.findOne({
             [ticketInfo.filter]: ticketInfo.val,
         }).exec();
     };
 
+type params = {
+    getTicketsByProjectId: {
+        ticketId: string;
+        page: number;
+        limit: number;
+    };
+};
+const getTicketsByProjectId =
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
+    async ({ ticketId, page, limit }: params["getTicketsByProjectId"]) => {
+        const tickets = await Tickets.paginate(
+            { ticketId: ticketId },
+            { page: page, limit: limit }
+        );
+
+        return tickets;
+    };
+
 const assignUserToTicket =
-    (Tickets: typeof Model <ticketType>) => async (ticketId: String, userId: String) => {
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
+    async (ticketId: String, userId: String) => {
         /*
-        *   assigning dev to a ticker 
-        */
+         *   assigning dev to a ticker
+         */
         const ticket = await Tickets.findOne({ ticketId: ticketId }).exec();
         // getting the users arr and adding id
-        const users: String[] = ticket.assignedDev || [];
+
+        const users: String[] = (ticket && ticket.assignedDev) || [];
         users.push(userId);
 
         const updatedTicket = await Tickets.updateOne(
@@ -53,13 +80,14 @@ const assignUserToTicket =
     };
 
 const removeUserFromTicket =
-    (Tickets: typeof Model <ticketType>) => async (ticketId: String, userId: String) => {
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
+    async (ticketId: String, userId: String) => {
         /*
-         * removing an assigned dev 
+         * removing an assigned dev
          */
         const ticket = await Tickets.findOne({ ticketId: ticketId }).exec();
         // finding users arr and removing user
-        const users: String[] = ticket.assignedDev || [];
+        const users: String[] = (ticket && ticket.assignedDev) || [];
         const filteredUsers = users.filter((user) => user != userId);
 
         const updatedTicket = await Tickets.updateOne(
@@ -70,25 +98,27 @@ const removeUserFromTicket =
         return updatedTicket.acknowledged;
     };
 
-const getStatistics = (Tickets: typeof Model <ticketType>) => async (projectIds: string[]) => {
-    /*
-    * Getting back status, severity and type from a project for each group 
-    */
-    const ticketsArr = [];
+const getStatistics =
+    (Tickets: mongoose.PaginateModel<ticketType>) =>
+    async (projectIds: string[]) => {
+        /*
+         * Getting back status, severity and type from a project for each group
+         */
+        const ticketsArr = [];
 
-    for (let i = 0; i < projectIds.length; i++) {
-        const tickets = await Tickets.find(
-            { projectId: projectIds[i] },
-            "ticketStatus ticketSeverity ticketType projectId"
-        ).exec();
-        ticketsArr.push(...tickets);
-    }
-    return ticketsArr;
-};
+        for (let i = 0; i < projectIds.length; i++) {
+            const tickets = await Tickets.find(
+                { projectId: projectIds[i] },
+                "ticketStatus ticketSeverity ticketType projectId"
+            ).exec();
+            ticketsArr.push(...tickets);
+        }
+        return ticketsArr;
+    };
 
 // TODO filter function
 
-export default (Ticket: typeof Model <ticketType>) => {
+export default (Ticket: mongoose.PaginateModel<ticketType>) => {
     return {
         createTicket: createTicket(Ticket),
         deleteTicket: deleteTicket(Ticket),
@@ -97,5 +127,6 @@ export default (Ticket: typeof Model <ticketType>) => {
         assignUserToTicket: assignUserToTicket(Ticket),
         removeUserFromTicket: removeUserFromTicket(Ticket),
         getStatistics: getStatistics(Ticket),
+        getTicketsByProjectId: getTicketsByProjectId(Ticket),
     };
 };
