8bee693097b3f23a13c5c25e93c7c3f822c9ea37
mrboparai <karanvirboparai123@gmail.com>
2022-07-13 12:17:06 -0400

    added pagination type

--- src/Services/Groups/group_service.ts
+++ src/Services/Groups/group_service.ts
@@ -1,32 +1,45 @@
 import { groupType } from "../../Model/Groups";
-import {Model} from 'mongoose';
+import mongoose, { Model } from "mongoose";
 
 const createGroup =
-    (Groups: typeof Model<groupType>) =>
+    (Groups: mongoose.PaginateModel<groupType>) =>
     async (groupInfo: groupType): Promise<groupType> => {
         const group = new Groups(groupInfo);
         return await group.save();
     };
 
-const updateGroup = (Groups: typeof Model<groupType>) => async (groupId: String, updates: {}) => {
-    const updatedGroup = await Groups.updateOne({ groupId: groupId }, updates);
-    // checking to see if it was updated and atleast 1 got updated
-    return updatedGroup.acknowledged && updatedGroup.matchedCount === 1;
-};
+const updateGroup =
+    (Groups: mongoose.PaginateModel<groupType>) =>
+    async (groupId: String, updates: {}) => {
+        const updatedGroup = await Groups.updateOne(
+            { groupId: groupId },
+            updates
+        );
+        // checking to see if it was updated and atleast 1 got updated
+        return updatedGroup.acknowledged && updatedGroup.matchedCount === 1;
+    };
 
-const deleteGroup = (Groups: typeof Model<groupType>) => async (groupId: String) => {
-    const deletedGroup = await Groups.deleteOne({
-        groupId: groupId,
-    }).exec();
-    // checking to see if it was deleted and atleast 1 got deleted
-    return deletedGroup.acknowledged && deletedGroup.deletedCount === 1;
-};
+const deleteGroup =
+    (Groups: mongoose.PaginateModel<groupType>) => async (groupId: String) => {
+        const deletedGroup = await Groups.deleteOne({
+            groupId: groupId,
+        }).exec();
+        // checking to see if it was deleted and atleast 1 got deleted
+        return deletedGroup.acknowledged && deletedGroup.deletedCount === 1;
+    };
 
-const getGroup = (Groups: typeof Model<groupType>) => async (groupInfo: {filter: "groupId" | "groupInviteCode", val: string}) => {
-    return await Groups.findOne({ [groupInfo.filter]: groupInfo.val }).exec();
-};
+const getGroup =
+    (Groups: mongoose.PaginateModel<groupType>) =>
+    async (groupInfo: {
+        filter: "groupId" | "groupInviteCode";
+        val: string;
+    }) => {
+        return await Groups.findOne({
+            [groupInfo.filter]: groupInfo.val,
+        }).exec();
+    };
 
-export default (Group: typeof Model<groupType>) => {
+export default (Group: mongoose.PaginateModel<groupType>) => {
     return {
         createGroup: createGroup(Group),
         deleteGroup: deleteGroup(Group),
