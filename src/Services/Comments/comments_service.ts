939b7f4f291f6725dc8782e8c76f6355fa5578d0
mrboparai <karanvirboparai123@gmail.com>
2022-07-13 12:11:43 -0400

    added type

--- src/Services/Comments/comments_service.ts
+++ src/Services/Comments/comments_service.ts
@@ -1,31 +1,32 @@
-import { Model } from "mongoose";
+import mongoose, { Model } from "mongoose";
 import { commentType } from "../../Model/Comments";
 
 const createComment =
-    (Comments: typeof Model<commentType>) =>
+    (Comments: mongoose.PaginateModel<commentType>) =>
     async (commentInfo: commentType): Promise<{}> => {
         const comment = new Comments(commentInfo);
         return await comment.save();
     };
 
 const updateComment =
-    (Comments: typeof Model<commentType>) => async (commentId: String, updates: {}) => {
+    (Comments: mongoose.PaginateModel<commentType>) =>
+    async (commentId: String, updates: {}) => {
         const updatedComment = await Comments.updateOne(
             { commentId: commentId },
             updates
         );
         // checking if it was updated and matched one
-        return updatedComment.acknowledged && updatedComment.matchedCount === 1; 
+        return updatedComment.acknowledged && updatedComment.matchedCount === 1;
     };
 
 const updateReply = async (
     commentId: String,
     replyId: String,
-    Comments: typeof Model<commentType>
+    Comments: mongoose.PaginateModel<commentType>
 ) => {
-    /* 
-    * adds reply to a comment 
-    */
+    /*
+     * adds reply to a comment
+     */
     const comment = await Comments.findOne({
         commentId: commentId,
     }).exec();
@@ -42,38 +43,39 @@ const updateReply = async (
     return updatedComment.acknowledged && updatedComment.matchedCount === 1;
 };
 
-const deleteComment = (Comments: typeof Model<commentType>) => async (commentId: String) => {
-    const deletedComment = await Comments.deleteOne({
-        commentId: commentId,
-    }).exec();
-    return deletedComment.acknowledged && deletedComment.deletedCount === 1;
-};
+const deleteComment =
+    (Comments: mongoose.PaginateModel<commentType>) =>
+    async (commentId: String) => {
+        const deletedComment = await Comments.deleteOne({
+            commentId: commentId,
+        }).exec();
+        return deletedComment.acknowledged && deletedComment.deletedCount === 1;
+    };
 
-const getComment = (Comments: typeof Model<commentType>) => async (commentInfo: {filter: "commentId", val: string}) => {
-    return await Comments.findOne({
-        [commentInfo.filter]: commentInfo.val,
-    }).exec();
-};
+const getComment =
+    (Comments: mongoose.PaginateModel<commentType>) =>
+    async (commentInfo: { filter: "commentId"; val: string }) => {
+        return await Comments.findOne({
+            [commentInfo.filter]: commentInfo.val,
+        }).exec();
+    };
 
 const replyTo =
-    (Comments: typeof Model<commentType>) => async (commentId: String, commentInfo: commentType) => {
+    (Comments: mongoose.PaginateModel<commentType>) =>
+    async (commentId: String, commentInfo: commentType) => {
         /*
- * adds commentID to comment for a reply
- */
+         * adds commentID to comment for a reply
+         */
         const comment = new Comments(commentInfo);
-        const foundComment = await Comments.findOne({commentId: commentId});
+        const foundComment = await Comments.findOne({ commentId: commentId });
         // if the original comment isnt found return false
-        if(!foundComment) return false;
+        if (!foundComment) return false;
 
         const createdComment = await comment.save();
         if (!createdComment) return false;
 
         // update reply with the replyid
-        await updateReply(
-            commentId,
-            createdComment.commentId,
-            Comments
-        );
+        await updateReply(commentId, createdComment.commentId, Comments);
 
         const updatedComment = await Comments.findOne({
             commentId: commentId,
@@ -82,26 +84,32 @@ const replyTo =
         return updatedComment;
     };
 
-const getReplyIds = (Comments: typeof Model<commentType>) => async (commentId: String) => {
-    const replys = await Comments.findOne(
-        { commentId: commentId },
-        `reply`
-    ).exec();
-    return replys;
-};
+const getReplyIds =
+    (Comments: mongoose.PaginateModel<commentType>) =>
+    async (commentId: String) => {
+        const replys = await Comments.findOne(
+            { commentId: commentId },
+            `reply`
+        ).exec();
+        return replys;
+    };
 
-const getAllComments = (Comments: typeof Model<commentType>) => async (replyArr: string[]) => {
-    const commentsArr = [];
-    for (let i = 0; i < replyArr.length; i++) {
-        const replyId = replyArr[i];
-        const comment = await Comments.findOne({ commentId: replyId }).exec();
-        commentsArr.push(comment);
-    }
+const getAllComments =
+    (Comments: mongoose.PaginateModel<commentType>) =>
+    async (replyArr: string[]) => {
+        const commentsArr = [];
+        for (let i = 0; i < replyArr.length; i++) {
+            const replyId = replyArr[i];
+            const comment = await Comments.findOne({
+                commentId: replyId,
+            }).exec();
+            commentsArr.push(comment);
+        }
 
-    return commentsArr;
-};
+        return commentsArr;
+    };
 
-export default (Comment: typeof Model<commentType>) => {
+export default (Comment: mongoose.PaginateModel<commentType>) => {
     return {
         createComment: createComment(Comment),
         deleteComment: deleteComment(Comment),
