97ebd2215c24b1ac34da5550edbe0c27865339b3
mrboparai <karanvirboparai123@gmail.com>
2022-07-16 17:41:00 -0400

    added join functionality with rooms

--- sockets.ts
+++ sockets.ts
@@ -1,8 +1,16 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 interface SocketData {
     username: string;
 }
+
+type roomUsers = {
+    socketId: Set<string>;
+    status: string;
+};
+
+// const usersOnline: roomUsers[] = [];
+const rooms = new Map<string, Map<string, roomUsers>>();
 
 const socketListen = (app: any) => {
     // TODO add redis backend for caching users in room
@@ -13,17 +21,7 @@ const socketListen = (app: any) => {
     io.on("connection", (socket) => {
         socket.emit("connection", socket.id);
 
-        socket.on("joinRoom", (roomId, username) => {
-            if (!roomId) {
-                socket.emit("error", { message: "Room id was not given" });
-            }
-            // check if user is already in room;
-
-            // if not then add user to room
-            socket.data.username = username;
-            socket.join(roomId);
-            socket.emit("roomJoined", socket.rooms.has(roomId));
-        });
+        socket.on("joinRoom", userJoinsRoom(socket));
 
         // invalidating query for all users
         socket.on("invalidateQuery", (data) => {
@@ -38,4 +36,53 @@ const wrap = (middleware: any) => (socket: any, next: any) => {
     middleware(socket.request, {}, next);
 };
 
+function userJoinsRoom(
+    socket: Socket<DefaultEventsMap, any, SocketData, any>
+): (...args: any[]) => void {
+    return (roomId, username) => {
+        if (!roomId) {
+            socket.emit("error", { message: "Room id was not given" });
+        }
+
+        // check if user is already in room;
+        // if not then add user to room
+        if (!rooms.has(roomId)) {
+            const users = new Map<string, roomUsers>();
+            users.set(username, {
+                socketId: new Set<string>().add(socket.id),
+                status: "online",
+            });
+
+            rooms.set(roomId, users);
+
+            socket.join(roomId);
+            // emit room data back
+        } else {
+            const users = rooms.get(roomId);
+
+            if (users && !users.has(username)) {
+                users.set(username, {
+                    socketId: new Set<string>().add(socket.id),
+                    status: "online",
+                });
+                rooms.set(roomId, users);
+
+                socket.join(roomId);
+                // emit room data back
+            } else {
+                const user = users?.get(username);
+                const userSockets = user?.socketId;
+                if (users && userSockets && userSockets.size < 4) {
+                    userSockets.add(socket.id);
+
+                    users.set(username, user);
+                    rooms.set(roomId, users);
+
+                    socket.join(roomId);
+                }
+            }
+        }
+        socket.emit("roomJoined", socket.rooms.has(roomId));
+    };
+}
 export { socketListen, wrap };
