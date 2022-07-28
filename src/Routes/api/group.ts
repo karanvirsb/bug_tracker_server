c22651af489cd95e4f735b1272c44bbf2eb3f763
mrboparai <karanvirboparai123@gmail.com>
2022-07-27 21:40:16 -0400

    added new routes

--- src/Routes/api/group.ts
+++ src/Routes/api/group.ts
@@ -6,9 +6,13 @@ import {
     getGroup,
     updateGroup,
     deleteGroup,
+    refreshInviteCode,
+    updateGroupName,
 } from "../../Controllers/Api/groupController";
 
 router.route("/").post(createGroup).put(updateGroup).delete(deleteGroup);
+router.route("/rename").put(updateGroupName);
 router.route("/id").post(getGroup);
+router.route("/refresh").put(refreshInviteCode);
 
 export default router;
