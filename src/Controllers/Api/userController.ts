05ef253dee9616d8de99846a3b231883b8f151f8
karanvirsb <karanvirboparai123@gmail.com>
2022-07-29 18:21:52 -0400

    adjusted tab spacing

--- src/Controllers/Api/userController.ts
+++ src/Controllers/Api/userController.ts
@@ -6,187 +6,171 @@ import generate from "../../Helper/generateId";
 import axios from "axios";
 
 const getUser = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response<any, Record<string, any>> | undefined> => {
-    const { filterValue, filter } = req.body;
-    if (!filterValue) throw Error("Invalid parameter");
+  const { filterValue, filter } = req.body;
+  if (!filterValue) throw Error("Invalid parameter");
 
-    try {
-        const user = await UserService.getUser({
-            filter: filter ?? "username",
-            val: filterValue,
-        });
-        if (!user) {
-            return res.sendStatus(204);
-        }
-        return res.status(200).json(user);
-    } catch (error) {
-        next(error);
+  try {
+    const user = await UserService.getUser({
+      filter: filter ?? "username",
+      val: filterValue,
+    });
+    if (!user) {
+      return res.sendStatus(204);
     }
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getUserByRefreshToken = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response<any, Record<string, any>> | undefined> => {
-    const { token } = req.body;
-    if (!token) return res.sendStatus(401);
-    try {
-        const user = await UserService.getUserByRefreshToken(token);
-        if (!user) {
-            return res.sendStatus(204);
-        }
-        return res.status(200).json(user);
-    } catch (error) {
-        next(error);
+  const { token } = req.body;
+  if (!token) return res.sendStatus(401);
+  try {
+    const user = await UserService.getUserByRefreshToken(token);
+    if (!user) {
+      return res.sendStatus(204);
     }
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getAllUsers = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response<any, Record<string, any>> | undefined> => {
-    const { users } = req.body;
+  const { users } = req.body;
 
-    if (!users) return res.sendStatus(401);
+  if (!users) return res.sendStatus(401);
 
-    try {
-        const foundUsers = await UserService.getAllUsers(users);
+  try {
+    const foundUsers = await UserService.getAllUsers(users);
 
-        if (!foundUsers) return res.sendStatus(204);
+    if (!foundUsers) return res.sendStatus(204);
 
-        return res.status(200).json(foundUsers);
-    } catch (error) {
-        next(error);
-    }
+    return res.status(200).json(foundUsers);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getUsersByGroupId = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response<any, Record<string, any>> | undefined> => {
-    const { groupId } = req.body;
+  const { groupId } = req.body;
 
-    if (!groupId) return res.sendStatus(401);
+  if (!groupId) return res.sendStatus(401);
 
-    try {
-        const usersArr = await UserService.getUsersByGroupId(groupId);
+  try {
+    const usersArr = await UserService.getUsersByGroupId(groupId);
 
-        if (!usersArr) return res.sendStatus(204);
+    if (!usersArr) return res.sendStatus(204);
 
-        return res.status(200).json(usersArr);
-    } catch (error) {
-        next(error);
-    }
+    return res.status(200).json(usersArr);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const createUser = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response<any, Record<string, any>> | undefined> => {
-    const user: UserType = (req as any).body.user;
-    // having predetermined colors for avatars
-    const colors = [
-        "d28100",
-        "d1423f",
-        "dc1677",
-        "c233a0",
-        "6163e1",
-        "246db6",
-        "008290",
-        "7ba100",
-        "9355d2",
-        "627a89",
-    ];
+  const user: UserType = (req as any).body.user;
+  // having predetermined colors for avatars
+  const colors = [
+    "d28100",
+    "d1423f",
+    "dc1677",
+    "c233a0",
+    "6163e1",
+    "246db6",
+    "008290",
+    "7ba100",
+    "9355d2",
+    "627a89",
+  ];
 
-    try {
-        // generating an id
-        let generatedId = await generate();
-        let foundUser = await UserService.getUser({
-            filter: "userId",
-            val: generatedId,
-        });
+  try {
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    const response = await axios(
+      `https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}&background=${color}&color=fff&length=2&rounded=true&bold=true&format=svg`,
+      { method: "get" }
+    );
+    // adding avatar to user
+    user["avatar"] = {
+      data: response.data,
+      contentType: "image/svg+xml",
+    };
+    // parse user to see if its correct
+    await IUser.parseAsync(user);
+    const newUser = await UserService.createUser(user);
+    if (newUser) return res.sendStatus(200);
 
-        // if we keep finding user keep generating key
-        while (foundUser) {
-            generatedId = await generate();
-            foundUser = await UserService.getUser({
-                filter: "userId",
-                val: generatedId,
-            });
-        }
-        user["userId"] = generatedId;
-        const color = colors[Math.floor(Math.random() * colors.length)];
-        const response = await axios(
-            `https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}&background=${color}&color=fff&length=2&rounded=true&bold=true&format=svg`,
-            { method: "get" }
-        );
-        // adding avatar to user
-        user["avatar"] = {
-            data: response.data,
-            contentType: "image/svg+xml",
-        };
-        // parse user to see if its correct
-        await IUser.parseAsync(user);
-        const newUser = await UserService.createUser(user);
-        if (newUser) return res.sendStatus(200);
-
-        return res.sendStatus(204); // if something goes wrong
-    } catch (error) {
-        if (error instanceof ZodError) {
-            return res.status(400).json({ message: error.message });
-        }
-        next(error);
+    return res.sendStatus(204); // if something goes wrong
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({ message: error.message });
     }
+    next(error);
+  }
 };
 
 const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.body;
-    try {
-        const deletedUser = await UserService.deleteUser(id);
+  const { id } = req.body;
+  try {
+    const deletedUser = await UserService.deleteUser(id);
 
-        if (deletedUser) return res.sendStatus(200);
-        return res.sendStatus(204);
-    } catch (error) {
-        next(error);
-    }
+    if (deletedUser) return res.sendStatus(200);
+    return res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const updateUser = async (req: Request, res: Response, next: NextFunction) => {
-    const { id, updates } = req.body;
-    const updatesKeys = Object.keys(updates);
+  const { id, updates } = req.body;
+  const updatesKeys = Object.keys(updates);
 
-    // looking for update keys and see if the exist in user
-    for (let i = 0; i < updatesKeys.length; i++) {
-        if (!IUser._getCached().keys.includes(updatesKeys[i])) {
-            return res.status(400).json({
-                message: `Update ${updatesKeys[i]} does not exist`,
-            });
-        }
+  // looking for update keys and see if the exist in user
+  for (let i = 0; i < updatesKeys.length; i++) {
+    if (!IUser._getCached().keys.includes(updatesKeys[i])) {
+      return res.status(400).json({
+        message: `Update ${updatesKeys[i]} does not exist`,
+      });
     }
-    try {
-        const updatedUser = await UserService.updateUser(id, updates);
+  }
+  try {
+    const updatedUser = await UserService.updateUser(id, updates);
 
-        if (updatedUser) return res.sendStatus(200);
+    if (updatedUser) return res.sendStatus(200);
 
-        return res.sendStatus(204);
-    } catch (error) {
-        next(error);
-    }
+    return res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export {
-    getUser,
-    createUser,
-    deleteUser,
-    updateUser,
-    getUserByRefreshToken,
-    getAllUsers,
-    getUsersByGroupId,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  getUserByRefreshToken,
+  getAllUsers,
+  getUsersByGroupId,
 };
--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -7,84 +7,88 @@ import UserService from "../Services/Users";
 
 // created object to check if given request are right
 const User = z.object({
-    username: z.string().min(4).max(26),
-    password: z.string().min(8).max(26),
+  username: z.string().min(4).max(26),
+  password: z.string().min(8).max(26),
 });
 
 const handleLogin = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    console.log(username, password);
-    if (!username || !password) {
-        return res
-            .status(400)
-            .json({ message: "Username and Password are required" });
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Username and Password are required" });
+  }
+  try {
+    // parsing user
+    await User.parseAsync({ username, password });
+
+    const foundUser = await UserService.getUser({
+      filter: "username",
+      val: username,
+    });
+
+    if (!foundUser) {
+      return res.sendStatus(401);
     }
-    try {
-        // parsing user
-        await User.parseAsync({ username, password });
 
-        const foundUser = await UserService.getUser({
-            filter: "username",
-            val: username,
-        });
-        if (!foundUser) {
-            return res.sendStatus(401);
-        }
+    const match = await bcrypt.compare(password, foundUser.password);
+    //evaulte the password
+    if (match) {
+      // create JWTs
+      let roles = [];
+      let roleKeys: keyof typeof foundUser.roles;
 
-        const match = await bcrypt.compare(password, foundUser.password);
-        //evaulte the password
-        if (match) {
-            // create JWTs
-            let roles = [];
-            let roleKeys: keyof typeof foundUser.roles;
+      for (roleKeys in foundUser.roles) {
+        roles.push(foundUser.roles[roleKeys]);
+      }
 
-            for (roleKeys in foundUser.roles) {
-                roles.push(foundUser.roles[roleKeys]);
-            }
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            username: foundUser.username,
+            roles,
+            group_id: foundUser.groupId,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET!,
+        { expiresIn: "15m" }
+      );
 
-            const accessToken = jwt.sign(
-                {
-                    UserInfo: {
-                        username: foundUser.username,
-                        roles,
-                        group_id: foundUser.groupId,
-                    },
-                },
-                process.env.ACCESS_TOKEN_SECRET!,
-                { expiresIn: "15m" }
-            );
+      const refreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET!,
+        { expiresIn: "1d" }
+      );
 
-            const refreshToken = jwt.sign(
-                { username: foundUser.username },
-                process.env.REFRESH_TOKEN_SECRET!,
-                { expiresIn: "1d" }
-            );
+      // sending refresh token through cookie for authentication
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "none",
+        secure: true,
+      });
 
-            // sending refresh token through cookie for authentication
-            res.cookie("jwt", refreshToken, {
-                httpOnly: true,
-                maxAge: 24 * 60 * 60 * 1000,
-                sameSite: "none",
-                secure: true,
-            });
-            UserService.updateUser(foundUser.username, {
-                refreshToken: refreshToken,
-            });
-            return res.json({
-                accessToken: accessToken,
-                firstName: foundUser.firstName,
-                lastName: foundUser.lastName,
-                email: foundUser.email,
-                avatar: foundUser.avatar,
-            });
-        } else {
-            return res.sendStatus(401);
-        }
-    } catch (error) {
-        if (error instanceof ZodError) {
-            return res.status(401).json({ message: error.message });
-        }
+      UserService.updateUser(foundUser.username, {
+        refreshToken: refreshToken,
+      });
+
+      console.log("line 73 auth");
+      return res.json({
+        accessToken: accessToken,
+        firstName: foundUser.firstName,
+        lastName: foundUser.lastName,
+        email: foundUser.email,
+        avatar: foundUser.avatar,
+      });
+    } else {
+      return res.sendStatus(401);
     }
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ZodError) {
+      return res.status(401).json({ message: error.message });
+    }
+  }
 };
 
 export { handleLogin };
