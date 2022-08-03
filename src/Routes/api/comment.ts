5fda5646b55ac198229b3679f9149d5afcd25683
mrboparai <karanvirboparai123@gmail.com>
2022-08-03 11:00:38 -0400

    added a get route for getting tickets by id

--- src/Routes/api/comment.ts
+++ src/Routes/api/comment.ts
@@ -9,10 +9,12 @@ import {
     replyTo,
     getReplyIds,
     getAllComments,
+    getCommentsByTicketId,
 } from "../../Controllers/Api/commentController";
 
 router.route("/").post(createComment).put(updateComment).delete(deleteComment);
 router.route("/id").post(getComment);
+router.route("/tickets/:id").get(getCommentsByTicketId);
 router.route("/reply").post(replyTo).get(getReplyIds);
 router.route("/reply/comments").post(getAllComments);
 
