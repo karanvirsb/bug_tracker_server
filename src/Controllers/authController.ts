304020cf41a0289565081a0ca9c423ab2369993f
mrboparai <karanvirboparai123@gmail.com>
2022-08-06 22:15:20 -0400

    added comment

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -7,88 +7,88 @@ import UserService from "../Services/Users";
 
 // created object to check if given request are right
 const User = z.object({
-  username: z.string().min(4).max(26),
-  password: z.string().min(8).max(26),
+    username: z.string().min(4).max(26),
+    password: z.string().min(8).max(26),
 });
 
 const handleLogin = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ message: "Username and Password are required" });
-  }
-  try {
-    // parsing user
-    await User.parseAsync({ username, password });
-
-    const foundUser = await UserService.getUser({
-      filter: "username",
-      val: username,
-    });
-
-    if (!foundUser) {
-      return res.sendStatus(401);
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ message: "Username and Password are required" });
     }
+    try {
+        // parsing user
+        await User.parseAsync({ username, password });
 
-    const match = await bcrypt.compare(password, foundUser.password);
-    //evaulte the password
-    if (match) {
-      // create JWTs
-      let roles = [];
-      let roleKeys: keyof typeof foundUser.roles;
+        const foundUser = await UserService.getUser({
+            filter: "username",
+            val: username,
+        });
 
-      for (roleKeys in foundUser.roles) {
-        roles.push(foundUser.roles[roleKeys]);
-      }
+        if (!foundUser) {
+            return res.sendStatus(401);
+        }
 
-      const accessToken = jwt.sign(
-        {
-          UserInfo: {
-            username: foundUser.username,
-            roles,
-            group_id: foundUser.groupId,
-          },
-        },
-        process.env.ACCESS_TOKEN_SECRET!,
-        { expiresIn: "15m" }
-      );
+        const match = await bcrypt.compare(password, foundUser.password);
+        //evaulte the password
+        if (match) {
+            // create JWTs
+            let roles = [];
+            let roleKeys: keyof typeof foundUser.roles;
 
-      const refreshToken = jwt.sign(
-        { username: foundUser.username },
-        process.env.REFRESH_TOKEN_SECRET!,
-        { expiresIn: "1d" }
-      );
+            // creating an array of roles rather than an object
+            for (roleKeys in foundUser.roles) {
+                roles.push(foundUser.roles[roleKeys]);
+            }
 
-      // sending refresh token through cookie for authentication
-      res.cookie("jwt", refreshToken, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000,
-        sameSite: "none",
-        secure: true,
-      });
+            const accessToken = jwt.sign(
+                {
+                    UserInfo: {
+                        username: foundUser.username,
+                        roles,
+                        group_id: foundUser.groupId,
+                    },
+                },
+                process.env.ACCESS_TOKEN_SECRET!,
+                { expiresIn: "15m" }
+            );
 
-      UserService.updateUser(foundUser.username, {
-        refreshToken: refreshToken,
-      });
+            const refreshToken = jwt.sign(
+                { username: foundUser.username },
+                process.env.REFRESH_TOKEN_SECRET!,
+                { expiresIn: "1d" }
+            );
 
-      console.log("line 73 auth");
-      return res.json({
-        accessToken: accessToken,
-        firstName: foundUser.firstName,
-        lastName: foundUser.lastName,
-        email: foundUser.email,
-        avatar: foundUser.avatar,
-      });
-    } else {
-      return res.sendStatus(401);
+            // sending refresh token through cookie for authentication
+            res.cookie("jwt", refreshToken, {
+                httpOnly: true,
+                maxAge: 24 * 60 * 60 * 1000,
+                sameSite: "none",
+                secure: true,
+            });
+
+            UserService.updateUser(foundUser.username, {
+                refreshToken: refreshToken,
+            });
+
+            return res.json({
+                accessToken: accessToken,
+                firstName: foundUser.firstName,
+                lastName: foundUser.lastName,
+                email: foundUser.email,
+                avatar: foundUser.avatar,
+            });
+        } else {
+            return res.sendStatus(401);
+        }
+    } catch (error) {
+        console.log(error);
+        if (error instanceof ZodError) {
+            return res.status(401).json({ message: error.message });
+        }
     }
-  } catch (error) {
-    console.log(error);
-    if (error instanceof ZodError) {
-      return res.status(401).json({ message: error.message });
-    }
-  }
 };
 
 export { handleLogin };
