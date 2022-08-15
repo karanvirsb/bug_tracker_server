1d71e037500bce0b922acd04f0610f7b882fdc6e
mrboparai <karanvirboparai123@gmail.com>
2022-08-15 11:15:30 -0400

    added projectIds to routes

--- src/Routes/api/project.ts
+++ src/Routes/api/project.ts
@@ -9,11 +9,13 @@ import {
     addUserToProject,
     removeUserFromProject,
     getAllProjectsByGroupId,
+    getProjectIdsByGroupId,
 } from "../../Controllers/Api/projectController";
 
 router.route("/").post(createProject).put(updateProject).delete(deleteProject);
 router.route("/id").post(getProject);
 router.route("/group/:id").get(getAllProjectsByGroupId);
+router.route("/group/projectIds/:id").get(getProjectIdsByGroupId);
 router.route("/user").post(addUserToProject).delete(removeUserFromProject);
 
 export default router;
