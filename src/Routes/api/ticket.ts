26fa547e554d2a37515d373a11f66c18686c323e
mrboparai <karanvirboparai123@gmail.com>
2022-08-15 11:15:41 -0400

    changed stats to post

--- src/Routes/api/ticket.ts
+++ src/Routes/api/ticket.ts
@@ -16,7 +16,7 @@ import {
 router.route("/").post(createTicket).put(updateTicket).delete(deleteTicket);
 router.route("/id").post(getTicket);
 router.route("/user").post(assignUserToTicket).delete(removeUserFromTicket);
-router.route("/stats").get(getStatistics);
+router.route("/stats").post(getStatistics);
 router.route("/project/:id").get(getTicketsByProjectId);
 router.route("/user/:username").get(getTicketsByUsername);
 
