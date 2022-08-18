7d5498baf5ac6a927ebd94c7d9a7f0cc807f95d9
mrboparai <karanvirboparai123@gmail.com>
2022-08-18 10:32:37 -0400

    added delete route

--- src/Routes/api/user.ts
+++ src/Routes/api/user.ts
@@ -9,12 +9,13 @@ import {
     updateUser,
     getAllUsers,
     getUsersByGroupId,
+    removeUserFromGroup,
 } from "../../Controllers/Api/userController";
 
 router.route("/").post(createUser);
 router.route("/id").post(getUser).put(updateUser).delete(deleteUser);
 router.route("/token").post(getUserByRefreshToken);
 router.route("/users").post(getAllUsers);
-router.route("/group").post(getUsersByGroupId);
+router.route("/group").post(getUsersByGroupId).delete(removeUserFromGroup);
 
 export default router;
