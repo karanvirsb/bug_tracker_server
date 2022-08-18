d3e5c3cddee5560d51ac9fd4b1eb0295098f0f34
mrboparai <karanvirboparai123@gmail.com>
2022-08-18 16:02:35 -0400

    protected api routes

--- src/Routes/api/project.ts
+++ src/Routes/api/project.ts
@@ -1,5 +1,7 @@
 import express from "express";
 const router = express.Router();
+import verifyRoles from "../../Middleware/verifyRoles";
+import rolesList from "../../Config/rolesList";
 
 import {
     createProject,
@@ -12,10 +14,20 @@ import {
     getProjectIdsByGroupId,
 } from "../../Controllers/Api/projectController";
 
-router.route("/").post(createProject).put(updateProject).delete(deleteProject);
+router
+    .route("/")
+    .post(createProject)
+    .put(verifyRoles(rolesList.ADMIN, rolesList.EDITOR), updateProject)
+    .delete(verifyRoles(rolesList.ADMIN, rolesList.EDITOR), deleteProject);
 router.route("/id").post(getProject);
 router.route("/group/:id").get(getAllProjectsByGroupId);
 router.route("/group/projectIds/:id").get(getProjectIdsByGroupId);
-router.route("/user").post(addUserToProject).delete(removeUserFromProject);
+router
+    .route("/user")
+    .post(verifyRoles(rolesList.ADMIN, rolesList.EDITOR), addUserToProject)
+    .delete(
+        verifyRoles(rolesList.ADMIN, rolesList.EDITOR),
+        removeUserFromProject
+    );
 
 export default router;
