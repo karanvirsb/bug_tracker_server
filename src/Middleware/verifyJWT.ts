e43bb0ca300a883c66e636b17ebf57f37f00ffa2
mrboparai <karanvirboparai123@gmail.com>
2022-09-17 16:27:21 -0400

    checking to see if it starts with and fixed jwt

--- src/Middleware/verifyJWT.ts
+++ src/Middleware/verifyJWT.ts
@@ -12,23 +12,34 @@ interface UserPayload {
 const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers["authorization"];
     // unauthorized
-    if (!authHeader) return next(new Error("Not authorized"));
+    if (!authHeader?.startsWith("Bearer")) return res.sendStatus(401);
 
     // Bearer token...
     const token = authHeader.split(" ")[1];
-    try {
-        // checking to see if the token has username, roles and groupId
-        const payload = jwt.verify(
-            token,
-            process.env.ACCESS_TOKEN_SECRET!
-        ) as UserPayload;
-        (req as any).user = payload?.UserInfo.username;
-        (req as any).roles = payload?.UserInfo.roles;
-        (req as any).groupId = payload?.UserInfo.groupId;
-        next();
-    } catch (error) {
-        return res.sendStatus(403); // could be tempered with
-    }
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET!,
+        (err: any, decoded: any) => {
+            if (err) return res.sendStatus(403); //invalid token
+            (req as any).user = decoded.UserInfo.username;
+            (req as any).roles = decoded.UserInfo.roles;
+            (req as any).groupId = decoded.UserInfo.groupId;
+            next();
+        }
+    );
+    // try {
+    //     // checking to see if the token has username, roles and groupId
+    //     const payload = jwt.verify(
+    //         token,
+    //         process.env.ACCESS_TOKEN_SECRET!
+    //     ) as UserPayload;
+    //     (req as any).user = payload?.UserInfo.username;
+    //     (req as any).roles = payload?.UserInfo.roles;
+    //     (req as any).groupId = payload?.UserInfo.groupId;
+    //     next();
+    // } catch (error) {
+    //     return res.sendStatus(403); // could be tempered with
+    // }
 };
 
 export default verifyJWT;
